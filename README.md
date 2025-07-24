6e7278aee22c600f6624a403631d91f973ec1f0a
Tim <49987073+timrosu@users.noreply.github.com>
2025-07-24 07:26:40 +0200

    working version

--- README.md
+++ README.md
@@ -1,33 +1,17 @@
-# horizont-cli
+# horizontium
  
-CLI interface for T-2 horizont portal
+CLI interface for automating T-2 horizont portal with selenium
 
 ## What does this do?
 
 Currently it allows setting redirection number for T-2 phone number.
 
-## Pipeline for setting redirections
+## Configuration
 
-1. send `username` and `password` headers to https://horizont.t-2.net/prijava and extract PHPSESSID cookie.
-
-2. get current settings by sending to https://horizont.t-2.net/nastavitve/telefonija forms below:
+Create `horizont.conf` file. Use template here:
 ```
-group: nastavitve
-handler: telefonija
-number_selector: '<t-2_number>'
-```
-
-3. set redirect number by sending to https://horizont.t-2.net/nastavitve/telefonija forms below:
-```
-group: nastavitve
-handler: telefonija
-action: preusmeritve
-number_selector: '<t-2_number>'
-CFU_menu: A
-CFU_c_number: '<your_number>'
-CFB_menu: D
-CFNR_menu: D
-CFNA_menu: D
-CW_menu: D
-shrani: Shrani
+username: <t-2_horizont(old_system)_username_from_pam>
+password: <t-2_horizont(old_system)_password_from_pam>
+t2_number: <same_as_username_in_our_case>
+redirect_number: <your_number_WITHOUT_1ST_DIGIT!>
 ```
--- horizontium.py
+++ horizontium.py
@@ -1,65 +1,108 @@
+from time import sleep
 from selenium import webdriver
-from selenium.webdriver.firefox.service import Service
 from selenium.webdriver.firefox.options import Options
 from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support.ui import Select
 from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.support.expected_conditions import presence_of_element_located
+
+def wait_for_page_to_load() -> None:
+    WebDriverWait(driver, 10).until(
+        lambda d: d.execute_script("return document.readyState") == "complete"
+    )
+
+def load_config(filename: str) -> dict:
+    """Load config from text file to dictionary. Originaly meant for http headers."""
+    config = {}
+    with open(filename, "r") as f:
+        for line in f:
+            if not line.strip() or ":" not in line:
+                continue
+            key, value = line.split(":", 1)
+            config[key.strip()] = value.strip()
+    return config
+
+
+###### CONFIG ######
+config = load_config("conf/horizont.conf")
+url = load_config("conf/urls.conf")
+####################
 
 
 # initialize web driver
 options = Options()
-# options.add_argument("--headless")  # Remove this line if you want to see the browser
+options.add_argument("--headless")  # Remove this line if you want to see the browser
 driver = webdriver.Firefox(options=options)
-base_url="https://horizont.t-2.net"
-
-
-###### CONFIG ######
-conf_t2_number = [redacted]
-conf_redirect_number = [redacted]
-####################
 
 
 # open login page
-driver.get(f"{base_url}/prijava")
+driver.get(url.get("prijava"))
+
+## scroll to the bottom
+driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
 
 ## wait for creds input field to be visible
-wait = WebDriverWait(driver, 10)
+wait = WebDriverWait(driver, 10, 0.5)
 username_input = wait.until(EC.presence_of_element_located((By.ID, "username_login")))
-password_input = driver.find_element(By.NAME, "password_login")
+password_input = driver.find_element(By.ID, "password_login")
 
 ## enter credentials
-username_input.send_keys("[redacted]")
-password_input.send_keys("[redacted]")
+wait_for_page_to_load()
+username_input.clear()
+username_input.send_keys(config.get("username"))
+password_input.clear()
+password_input.send_keys(config.get("password"))
 
 ## accept terms and conditions
+sleep(0.5)
 driver.find_element(By.ID, "strinjam_se_s_pogoji").click()
 
 ## find and click the login button
-driver.find_element(By.CSS_SELECTOR, ".primary").click()
+sleep(0.5)
+driver.find_element(By.CSS_SELECTOR, "button.login").click()
+
+
+# wait for page to load
+wait_for_page_to_load()
+# wait for the customer name div to appear
+WebDriverWait(driver, 10).until(
+    EC.presence_of_element_located((By.CSS_SELECTOR, "#expandDisplayName"))
+)
 
 
 # open settings page
-driver.get(f"{base_url}/nastavitve/telefonija")
+driver.get(url.get("telefonija"))
 
 ## select a desired phone number
-number_dropdown = driver.find_element(By.ID, "number_selector")
-Select(number_dropdown).select_by_value(f"label={conf_t2_number}")
+number_dropdown = wait.until(EC.presence_of_element_located((By.ID, "number_selector")))
+Select(number_dropdown).select_by_value(config.get("t2_number"))
 
 ## open Preusmeritve tab
-driver.find_element(By.LINK_TEXT, "Preusmeritve").click()
+number_dropdown = wait.until(
+    EC.presence_of_element_located((By.LINK_TEXT, "Preusmeritve"))
+).click()
 
 ## read existent unconditional redirect boolean-like entity and enable it
-redirect_enabled_menu = driver.find_element(By.NAME, "CFU_c_menu")
-if redirect_enabled_menu is "Ne":
-   Select(redirect_enabled_menu).select_by_value(f"Da")
+redirect_toggle = Select(driver.find_element(By.NAME, "CFU_menu"))
+
+print("old selected index:", redirect_toggle.options.index(redirect_toggle.first_selected_option))
+if redirect_toggle != "A":
+    redirect_toggle.select_by_index(0)
+    sleep(0.5)
+    print("new selected index:", redirect_toggle.options.index(redirect_toggle.first_selected_option))
 
 ## read existent unconditional redirect number
 redirect_number = driver.find_element(By.NAME, "CFU_c_number")
-if redirect_number is not conf_redirect_number:
-   redirect_number.send_keys(conf_redirect_number)
+old_value = redirect_number.get_attribute("value")
+print("old number:", old_value)
+if old_value != config.get("redirect_number"):
+    print("red-num different than in conf, setting user preferred")
+    redirect_number.clear()
+    redirect_number.send_keys(config.get("redirect_number"))
+    print("old number:", redirect_number.get_attribute("value"))
+    ## save changes
+    driver.find_element(By.NAME, "shrani").click()
 
-## save changes
-driver.find_element(By.NAME, "shrani").click()
+print("✅")
+sleep(2)
+driver.close()
