06fc748c7a95571a8cfd2e2e3d98cc60f341a3e3
Tim <49987073+timrosu@users.noreply.github.com>
2026-08-21 07:43:57 +0200

    converted to uv2nix

--- flake.nix
+++ flake.nix
@@ -1,40 +1,85 @@
 {
   description = "Flake using pyproject.toml metadata";
 
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-  inputs.pyproject-nix.url = "github:pyproject-nix/pyproject.nix";
-  inputs.pyproject-nix.inputs.nixpkgs.follows = "nixpkgs";
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+
+    pyproject-nix = {
+      url = "github:pyproject-nix/pyproject.nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    uv2nix = {
+      url = "github:pyproject-nix/uv2nix"
+      inputs.pyproject-nix.follows = "pyproject-nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    pyproject-build-systems = {
+      url = "github:pyproject-nix/build-system-pkgs"
+      inputs.pyproject-nix.follows = "pyproject-nix";
+      inputs.uv2nix.follows = "uv2nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  }
 
   outputs =
-    { self, nixpkgs, pyproject-nix, ... }:
+    { self, nixpkgs, pyproject-nix, uv2nix, pyproject-build-systems, ... }:
     let
       inherit (nixpkgs) lib;
       forAllSystems = lib.genAttrs lib.systems.flakeExposed;
 
-      project = pyproject-nix.lib.project.loadPyproject {
-        projectRoot = self;
+      workspace = uv2nix.lib.workspace.loadWorkspace { workspaceRoot = ./.; };
+
+      overjay = workspace.mkPyprojectOverlay {
+        sourcePreference = "wheel";
       };
+
+      editableOverlay = workspace.mkEditablePyprojectOverlay {
+        root = "$REPO_ROOT";
+      };
+
+      pythonSets = forAllSystems (system: let
+        pkgs = nixpkgs.legacyPackages.${system};
+        python = pkgs.python3;
+      in 
+        (pkgs.callPackage pyproject-nix.build.packages {
+          inherit python;
+        }).overrideScope
+        (
+          lib.composeManyExtensions [
+            pyproject-build-systems.overlays.wheel
+            overlay
+          ]
+        )
+      );
 
     in
     {
       devShells = forAllSystems (system: let
         pkgs = nixpkgs.legacyPackages.${system};
-        python = pkgs.python3;
-        arg = project.renderers.withPackages { inherit python; };
-        pythonEnv = python.withPackages arg;
+        pythonSet = pythonSets.${system}.overrideScope editableOverlay;
+        virtualenv = pythonSet.mkVirtualEnv "horizont-dev-env" workspace.deps.all;
       in {
         default =
           pkgs.mkShell {
-            packages = [ pythonEnv ];
+            packages = [ 
+              virtualenv
+              pkgs.uv
+            ];
+            env = {
+              UV_PYTHON = pkgs.python3.interpreter;
+            };
+            shellHook = ''
+              unset PYTHONPATH
+              export REPO_ROOT=$(readlink -d .)
+              . ${virtualenv}/bin/activate
+            ''
           };
         });
 
-      packages = forAllSystems (system: let
-          pkgs = nixpkgs.legacyPackages.${system};
-          python = pkgs.python3;
-          attrs = project.renderers.buildPythonPackage { inherit python; };
-        in {
-          default = python.pkgs.buildPythonPackage attrs;
+      packages = forAllSystems (system:  {
+          default = pythonSet.mkVirtualEnv "horizont-env" workspace.deps.default; 
         }
       );
     };
